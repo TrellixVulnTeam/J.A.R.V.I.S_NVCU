70e282ee313be723a65bf6ea94589780e6389897
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-05-30 11:07:22 +0530

    Text-To-Speech Working Properly

--- J.A.R.V.I.S.py
+++ J.A.R.V.I.S.py
@@ -1,4 +1,18 @@
-import pyttsx3
+import pyttsx3 #text-to-speech package
+
+#text-to-speech engine started
 engine = pyttsx3.init()
-engine.say("I will speak this text")
+
+# getting details of current speaking rate
+rate = engine.getProperty('rate')
+print(rate)
+# set details of current speaking rate (speech speed)
+engine.setProperty('rate', 200)
+
+#to get all voice types (Male Or Female)
+voices = engine.getProperty('voices')
+#voices[0] - for Male Voice
+#voices[1] - for Female Voice
+engine.setProperty('voice', voices[0].id)
+engine.say("Good Morning sir")
 engine.runAndWait()
